705270b5d467c9701e7f3376685605f774a5d891
Maxim Navrotskiy <navrotskyi.maksym@lll.kpi.ua>
2024-06-05 09:20:00 +0300

    Add files via upload

---
+++ WebApplication1/WebApplication1/Controllers/HotelControllers.cs
@@ -0,0 +1,153 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.DynamoDBv2.Model;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using WebApplication1.Modles;
+
+namespace WebApplication1.Controllers
+{   
+    [ApiController]
+    [Route("[controller]")]
+    public class HotelController : ControllerBase
+    {
+        readonly string _key = "1";
+        private readonly IAmazonDynamoDB _dynamoDbClient;
+        private readonly DynamoDBContext _context;
+
+        public HotelController()
+        {
+            var config = new AmazonDynamoDBConfig
+            {
+                RegionEndpoint = Amazon.RegionEndpoint.EUNorth1
+            };
+
+            _dynamoDbClient = new AmazonDynamoDBClient("2", " 2", config);
+            _context = new DynamoDBContext(_dynamoDbClient);
+        }
+
+        [HttpGet("sea" +
+            "rch")]
+        public async Task<IActionResult> GetSearchHotel([FromQuery] string keyword)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = $"https://api.content.tripadvisor.com/api/v1/location/search?key={_key}&searchQuery={keyword}&language=en";
+                var response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                string json = await response.Content.ReadAsStringAsync();
+                var hotelData = JsonConvert.DeserializeObject<Root>(json);
+
+                string jsonResponse = JsonConvert.SerializeObject(hotelData);
+                var content = new ContentResult
+                {
+                    Content = jsonResponse,
+                    ContentType = "application/json",
+                    StatusCode = 200
+                };
+
+                return content;
+            }
+        }
+
+        [HttpGet("details")]
+        public async Task<IActionResult> GetLocationDetails([FromQuery] string locationId)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = $"https://api.content.tripadvisor.com/api/v1/location/{locationId}/details?key={_key}";
+                var response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                string json = await response.Content.ReadAsStringAsync();
+
+                return Content(json, "application/json");
+            }
+        }
+
+        [HttpGet("photos")]
+        public async Task<IActionResult> GetLocationPhotos([FromQuery] string locationId)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = $"https://api.content.tripadvisor.com/api/v1/location/{locationId}/photos?key={_key}";
+                var response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                string json = await response.Content.ReadAsStringAsync();
+
+                return Content(json, "application/json");
+            }
+        }
+
+        [HttpPost("add")]
+        public async Task<IActionResult> AddHotel([FromBody] Hotel2 hotel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _context.SaveAsync(hotel);
+            return Ok(new { Message = "Hotel added successfully" });
+        }
+
+        [HttpGet("get")]
+        public async Task<IActionResult> GetHotel([FromQuery] string hotelName)
+        {
+            var hotel = await _context.LoadAsync<Hotel2>(hotelName);
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+            return Ok(hotel);
+        }
+
+        [HttpDelete("delete")]
+        public async Task<IActionResult> DeleteHotel([FromQuery] string hotelName)
+        {
+            var hotel = await _context.LoadAsync<Hotel2>(hotelName);
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+
+            await _context.DeleteAsync(hotel);
+            return Ok(new { Message = "Hotel deleted successfully" });
+        }
+    }
+
+    [DynamoDBTable("Travelgo")]
+    public class Hotel2
+    {
+        [DynamoDBHashKey]
+        [Required(ErrorMessage = "HotelName is required")]
+        public string Hotel { get; set; }
+
+        [DynamoDBProperty]
+        [Required(ErrorMessage = "City is required")]
+        public string City { get; set; }
+
+        [DynamoDBProperty]
+        [Required(ErrorMessage = "LocationId is required")]
+        public string LocationId { get; set; }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ WebApplication1/WebApplication1/Modles/Hotel.cs
@@ -0,0 +1,24 @@
+﻿namespace WebApplication1.Modles
+{
+    public class AddressObj
+    {
+        public string address_string;
+        public string city;
+        public string country;
+        public string postalcode;
+        public string street1;
+        public string street2;
+    }
+
+    public class Datum
+    {
+        public AddressObj address_obj;
+        public string location_id;
+        public string name;
+    }
+
+    public class Root
+    {
+        public List<Datum> data;
+    }
+}
